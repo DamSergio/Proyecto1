8ef21a42e2049fb3f0f6ba5468011bc3567f27f1
SergioBallesteros <dam_sergioballesteros@riberadeltajo.es>
2023-02-14 16:03:10 +0100

    cambio la clase principal

--- prueba1/src/paquete/Principal.java
+++ prueba1/src/paquete/Principal.java
@@ -3,6 +3,9 @@ package paquete;
 public class Principal {
 	
 	public static void main(String[] args) {
+		
 		System.out.println("hola mundo");
+		System.out.println("Cambio la clase principal");
+		
 	}
 }
