b2d8e7a457d3675852954e29576e1abbb8e3502a
SergioBallesteros <dam_sergioballesteros@riberadeltajo.es>
2023-02-14 16:03:44 +0100

    cosas feas al main

--- prueba1/src/paquete/Principal.java
+++ prueba1/src/paquete/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		System.out.println("hola mundo");
-		System.out.println("Cambio la clase principal");
+		System.out.println("Cambio la clase principal hasbdjkasjfkbasjbf");
 		
 	}
 }
